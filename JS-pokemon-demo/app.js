a24ee37e4d27b217a46d0eccfaa4cd9d30a205d4
Tony Leng <lengtk@gmail.com>
2023-03-08 23:46:19 +0900

    removed commented URL from app.js

--- JS-pokemon-demo/app.js
+++ JS-pokemon-demo/app.js
@@ -1,4 +1,3 @@
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
