1304531cea9bf53623b716b18db6c220481e4626
Tony Leng <lengtk@gmail.com>
2023-03-10 12:22:10 +0900

    background and h1 now change on button click

--- JS-random-color-generator/index.js
+++ JS-random-color-generator/index.js
@@ -1,10 +1,15 @@
 const h1 = document.querySelector('h1');
-const btn = document.querySelector('#btn');
-const r = Math.floor(Math.random(1) * 255);
-const g = Math.floor(Math.random(1) * 255);
-const b = Math.floor(Math.random(1) * 255);
+const button = document.querySelector('button');
 
-btn.onclick = () => {
-  h1.innerText = `rgb (${r}, ${g}, ${b})`;
-  document.body.style.backgroundColor = (r, g, b);
-};
+const makeRandColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`
+}
+
+button.addEventListener('click', () => {
+  const newColor = makeRandColor();
+  document.body.style.backgroundColor = newColor;
+  h1.innerText = newColor;
+});
