af13e5f8a88e5a491317037072ba3e3dac50029a
Tony Leng <lengtk@gmail.com>
2023-03-11 22:20:59 +0900

    randomized button text color, used this keyword to refactor and add a new function

--- JS-button-color-changer/index.js
+++ JS-button-color-changer/index.js
@@ -7,16 +7,11 @@ const randColor = () => {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-// const colorize = () => {
-//   document.button.style.backgroundColor = randColor;
-//   document.button.style.text = randColor;
-// }
+function colorize () {
+    this.style.backgroundColor = randColor();
+    this.style.color = randColor();
+}
 
 for (let button of buttons) {
-  button.addEventListener('click', () => {
-    button.style.backgroundColor = randColor();
-    button.style.text = randColor();
-    console.log("clicked!");
-    console.log(randColor());
-  });
+  button.addEventListener('click', colorize);
 }
