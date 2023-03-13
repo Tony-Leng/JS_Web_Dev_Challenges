50efe35225a0c722a498062df1d3dd5759a6637c
Tony Leng <lengtk@gmail.com>
2023-03-13 12:52:39 +0900

    enabled new item and quantity to be added to ul

---
+++ JS-form-submission-practice/app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form");
+const prd = document.querySelector("#product");
+const qty = document.querySelector("#qty");
+const list = document.querySelector("#list");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const prdInput = prd.value;
+  const qtyInput = qty.value;
+  const newLI = document.createElement("LI");
+  newLI.innerText = `${qtyInput} ${prdInput}`;
+  list.append(newLI);
+})
