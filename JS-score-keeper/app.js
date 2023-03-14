24c4523340952594ac846b89c44d531119c1983a
Tony Leng <lengtk@gmail.com>
2023-03-14 11:17:26 +0900

    added winning score selector

--- JS-score-keeper/app.js
+++ JS-score-keeper/app.js
@@ -1,8 +1,9 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
-const p1Display = document.querySelector('#p1Display')
-const p2Display = document.querySelector('#p2Display')
-const reset = document.querySelector('#reset')
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -30,10 +31,17 @@ p2Button.addEventListener('click', function () {
   }
 })
 
-reset.addEventListener('click', function() {
+winningScoreSelect.addEventListener('change', function() {
+  winningScore = parseInt(this.value);
+  reset();
+} )
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
   isGameOver = false;
   p1Score = 0;
   p2Score = 0;
   p1Display.textContent = 0;
   p2Display.textContent = 0;
-})
+}
