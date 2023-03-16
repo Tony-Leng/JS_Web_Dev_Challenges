bcef996ebbd47c3df3f372148c757c845cd84816
Tony Leng <lengtk@gmail.com>
2023-03-16 16:16:54 +0900

    created delayedColorChange promise

---
+++ JS-promises-async-await/app.js
@@ -0,0 +1,8 @@
+const delayedColorChange = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, 1000);
+  })
+}
