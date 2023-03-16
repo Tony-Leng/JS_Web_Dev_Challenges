9432788f3e530bea576c0ecad274593008fef8f9
Tony Leng <lengtk@gmail.com>
2023-03-16 16:25:29 +0900

    added async function with await keyword

--- JS-promises-async-await/app.js
+++ JS-promises-async-await/app.js
@@ -3,6 +3,15 @@ const delayedColorChange = (color, delay) => {
     setTimeout(() => {
       document.body.style.backgroundColor = color;
       resolve();
-    }, 1000);
+    }, delay);
   })
+}
+
+async function colorChange() {
+  await delayedColorChange('red', 1000);
+  await delayedColorChange('orange', 1000);
+  await delayedColorChange('yellow', 1000);
+  await delayedColorChange('green', 1000);
+  await delayedColorChange('indigo', 1000);
+  await delayedColorChange('violet', 1000);
 }
